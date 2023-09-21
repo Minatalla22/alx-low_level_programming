7c0baea93df834595fcb82ca56fb5bca8f2e9a2e
Minatalla22 <menabashir1964@gmail.com>
2023-09-21 01:48:08 -0700

    done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  *string_toupper - letter
+ *   *
+ *    *@s:awdwad
+ *      *
+ *       * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
